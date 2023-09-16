3faab02510c89659c7e3572019630127c6e46c82
Gunalan <dragunalan@gmail.com>
2023-09-16 16:47:52 +0700

    add project

---
+++ Backend/XtramileWeather/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XtramileWeather.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        [HttpGet("weather-city")]
+        public async Task<ActionResult> Index(string city)
+        {
+            return Ok();
+        }
+    }
+}
