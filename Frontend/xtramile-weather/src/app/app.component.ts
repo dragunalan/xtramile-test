68afe37ba51804e64475f28de7afafa2ef46ebcc
Gunalan <dragunalan@gmail.com>
2023-09-17 21:06:04 +0700

    add completed project

--- Frontend/xtramile-weather/src/app/app.component.ts
+++ Frontend/xtramile-weather/src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { WeatherResponse, Wind } from './weather-object';
 
+/**
+ * @title Basic select
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports:[MatSelectModule, CommonModule, FormsModule, ReactiveFormsModule]
 })
-export class AppComponent {
-  title = 'xtramile-weather';
+export class AppComponent implements OnInit  {
+    constructor(private http: HttpClient) {}
+    countries : any = [];
+    cities : any = [];
+    city = new FormControl('', Validators.required);
+    weatherResponse: WeatherResponse = {} as WeatherResponse
+    
+    ngOnInit():void
+    {
+      fetch('./assets/countries+cities.json').then(res => res.json())
+    .then(jsonData => {
+        jsonData.forEach((element: any) => {
+            this.countries.push({id: element.id, name : element.name});
+        })
+    });
+    }
+
+    fillCities(value : any)
+    {
+        this.cities = [];
+        fetch('./assets/countries+cities.json').then(res => res.json())
+      .then(jsonData => {
+          jsonData.forEach((element: any) => {
+              if(element.id === value)
+              {
+                  element.cities.forEach((cit: any) =>
+                  {
+                    this.cities.push({name : cit.name});
+                  })
+              }
+          })
+        });
+    }
+
+    onClickSubmit(data:any) {
+      var headers = new HttpHeaders()
+      
+      headers.append("Access-Control-Allow-Origin", "*");
+      headers.append("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+      headers.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+      
+       this.http.get<any>('http://localhost:5000/weather-city?city=' + this.city.value, {headers} ).subscribe((data : WeatherResponse) => {
+        this.weatherResponse = data;
+       })
+   }
 }
--- Frontend/xtramile-weather/src/app/app.module.ts
+++ Frontend/xtramile-weather/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations()
+  ]
+});
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    AppComponent,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
